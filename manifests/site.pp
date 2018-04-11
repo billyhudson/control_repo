4be9351327dab79e19d13a02d40d1a6506b9cc6b
billyhudson <billy.hudson@primarysite.net>
2018-04-11 18:04:15 +0100

    what

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,10 @@
 node default {
   file {'/root/README':
     ensure  => file,
-    content => 'This is a README',
+    content => 'This is a README that was provisioned with puppet!',
   }
-  
+}
+
+node 'master.puppet.vm' {
+  include role::master_server
 }
